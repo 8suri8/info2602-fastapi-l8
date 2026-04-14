091bcb33b0f429ab12594d7430571f91341ecc26
Suri Dass <suridass2020@gmail.com>
2026-04-13 23:25:38 -0400

    Enhance stats router with todo statistics endpoint
    Added a new endpoint for todo statistics and updated the stats router with tags.

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -9,13 +9,16 @@ from typing import Annotated
 from fastapi import status
 from . import templates
 
-stats_router = APIRouter()
+stats_router = APIRouter(tags=["Statistics"])
 
 @stats_router.get("/stats", response_class=HTMLResponse)
 async def stats_page(
     request: Request,
     user: AdminDep
 ):
+    """
+    Render the statistics page (admin only)
+    """
     return templates.TemplateResponse(
         request=request, 
         name="stats.html",
@@ -24,3 +27,26 @@ async def stats_page(
             "current_user": user
         }
     )
+
+@stats_router.get("/todo-stats")
+async def todo_stats(
+    request: Request,
+    user: AdminDep,
+    db: SessionDep
+):
+    """
+    API endpoint that returns todo statistics grouped by user
+    Returns a dictionary with usernames as keys and todo counts as values
+    """
+    # Get all todos
+    todos = db.exec(select(Todo)).all()
+    
+    # Count todos per user
+    res = {}
+    for todo in todos:
+        if todo.user.username in res:
+            res[todo.user.username] += 1
+        else:
+            res[todo.user.username] = 1
+    
+    return res
